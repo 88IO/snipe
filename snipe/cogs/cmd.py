16afe456edd094f86e8ddb89754a60aab0798c13
88IO <8810aoba@gmail.com>
2021-04-18 20:06:48 +0900

    バグ修正

--- snipe/cogs/cmd.py
+++ snipe/cogs/cmd.py
@@ -13,15 +13,19 @@ class CmdCog(commands.Cog):
         self.vc = None
         self.loop.start()
 
-    @tasks.loop(seconds=10.0)
+    @tasks.loop(seconds=3)
     async def loop(self):
         print("loop...")
-        print(isinstance(self.tasks[0].member if self.tasks else None, discord.Member))
+        print(self.tasks)
+
         while self.tasks and self.tasks[0].datetime <= datetime.datetime.now():
             task = hq.heappop(self.tasks)
             if task.member.voice:
-                await task.member.send(f"{task.member.display_name}を切断しました")
-                await task.member.move_to(None)
+                if task.type == Task.DISCONNECT:
+                    await task.member.send(f"{task.datetime}に通話を強制切断しました")
+                    await task.member.move_to(None)
+                elif task.type == Task.BEFORE_3MIN:
+                    await task.member.send("3分後に通話を強制切断します")
 
     @commands.group()
     async def snipe(self, ctx):
@@ -30,79 +34,73 @@ class CmdCog(commands.Cog):
 
     @snipe.command()
     async def show(self, ctx):
+        embed = discord.Embed(title="通話切断予約", description=f"snipe botによる通話切断予約表です")
         for task in sorted(self.tasks):
-            await ctx.send(f"{task.datetime}: {task.member.display_name}")
+            embed.add_field(name=str(task.datetime),
+                value=("強制切断" if task.type == Task.DISCONNECT else "3分前連絡") + f": {task.member.display_name}")
+        await ctx.send(embed=embed)
 
     @snipe.command()
     async def connect(self, ctx):
         if ctx.author.voice:
             self.vc = await ctx.author.voice.channel.connect()
-            self.vc.play(discord.FFmpegPCMAudio("snipe/sounds/connect.wav"), after=lambda e: print("connected"))
+            self.vc.play(discord.FFmpegPCMAudio("snipe/sounds/connect.wav"), after=lambda _: print("connected"))
 
     @snipe.command()
     async def disconnect(self, ctx):
         print("call disconnect()")
         if self.vc.is_connected():
-            self.vc.play(discord.FFmpegPCMAudio("snipe/sounds/disconnect.wav"), after=lambda e: print("disconnected"))
+            self.vc.play(discord.FFmpegPCMAudio("snipe/sounds/disconnect.wav"), after=lambda _: print("disconnected"))
             await self.vc.disconnect()
 
     @snipe.command()
-    async def reserve(self, ctx, args, member: discord.Member):
-        if re.fullmatch("[0-9]{1,2}:[0-9]{1,2}", args):
-            d = datetime.datetime.strptime(args, "%M:%S")
-            if d < datetime.datetime.now().time():
-                new_task = Task(
-                    datetime.datetime.now().replace(
-                        minutes=d.minute, seconds=d.second, microsecond=0)
-                    + datetime.timedelta(hours=1),
-                    member if member else ctx.member
-                )
-            else:
-                new_task = Task(
-                    datetime.datetime.now().replace(
-                        minutes=d.minute, seconds=d.second, microsecond=0),
-                    member if member else ctx.member
-                )
-        elif re.fullmatch("[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}", args):
-            d = datetime.datetime.strptime(args, "%H:%M:%S")
-            if d < datetime.datetime.now().time():
-                new_task = Task(
-                    datetime.datetime.now().replace(
-                        hours=d.hour, minutes=d.minute, seconds=d.second, microsecond=0)
-                    + datetime.timedelta(days=1),
-                    member if member else ctx.member
-                )
-            else:
-                new_task = Task(
-                    datetime.datetime.now().replace(
-                        hours=d.hour, minutes=d.minute, seconds=d.second, microsecond=0),
-                    member if member else ctx.member
-                )
-        else:
-            return
-        hq.heappush(self.tasks, new_task)
-        await ctx.send(f"{new_task.datetime}に{new_task.member.display_name}を切断します")
+    async def reserve(self, ctx, *args):
+        t = " ".join(map(str, args))
+        if match := re.fullmatch(r"^(?:(?P<hour>\d{1,2})(?:時間|時|:|：|h|H|\s+))?"\
+                                 + r"(?:(?P<minute>\d{1,2})(?:分|m|M|))?.*", t):
+            hour, minute = map(lambda x: int(x) if x else 0, match.group("hour", "minute"))
+            now = datetime.datetime.now()
+            disconnect_task = Task(
+                now.replace(hour=hour, minute=minute, microsecond=0)
+                + datetime.timedelta(
+                    days=int(datetime.time(hour=hour, minute=minute) < now.time())),
+                ctx.member,
+                Task.DISCONNECT)
+            hq.heappush(self.tasks, disconnect_task)
+
+            if disconnect_task.datetime - now > datetime.timedelta(minutes=3):
+                before3min_task = Task(
+                    disconnect_task.datetime - datetime.timedelta(minutes=3),
+                    ctx.member,
+                    Task.BEFORE_3MIN)
+                hq.heappush(self.tasks, before3min_task)
+
+            await ctx.send(f"{disconnect_task.datetime}に"
+                           + f"{disconnect_task.member.display_name}を切断します")
 
     @snipe.command()
-    async def reservein(self, ctx, args, member: discord.Member):
-        if re.fullmatch("[0-9]{1,2}:[0-9]{1,2}", args):
-            d = datetime.datetime.strptime(args, "%M:%S")
-            new_task = Task(
+    async def reservein(self, ctx, *args):
+        t = " ".join(map(str, args))
+        if match := re.fullmatch(r"^(?:(?P<hour>\d{1,2})(?:時間|時|:|：|h|H|\s+))?"\
+                                 + r"(?:(?P<minute>\d{1,2})(?:分|m|M|))?.*", t):
+            hour, minute = map(lambda x: int(x) if x else 0, match.group("hour", "minute"))
+            disconnect_task = Task(
                 datetime.datetime.now().replace(microsecond=0)
-                + datetime.timedelta(minutes=d.minute, seconds=d.second),
-                member if member else ctx.member
-            )
-        elif re.fullmatch("[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}", args):
-            d = datetime.datetime.strptime(args, "%H:%M:%S")
-            new_task = Task(
-                datetime.datetime.now().replace(microsecond=0)
-                + datetime.timedelta(hours=d.hour, minutes=d.minute, seconds=d.second),
-                member if member else ctx.member
-            )
-        else:
-            return
-        hq.heappush(self.tasks, new_task)
-        await ctx.send(f"{new_task.datetime}に{new_task.member.display_name}を切断します")
+                  + datetime.timedelta(hours=hour, minutes=minute),
+                ctx.member,
+                Task.DISCONNECT)
+            hq.heappush(self.tasks, disconnect_task)
+
+            if hour * 60 + minute > 3:
+                before3min_task = Task(
+                    datetime.datetime.now().replace(microsecond=0)
+                    + datetime.timedelta(hours=hour, minutes=minute-3),
+                    ctx.member,
+                    Task.BEFORE_3MIN)
+                hq.heappush(self.tasks, before3min_task)
+
+            await ctx.send(f"{disconnect_task.datetime}に"
+                           + f"{disconnect_task.member.display_name}を切断します")
 
 
 def setup(bot):
--- snipe/task.py
+++ snipe/task.py
@@ -1,12 +1,16 @@
 from discord import Member, User
 from datetime import datetime as Datetime
-import typing
 
 
 class Task:
-    def __init__(self, datetime, member):
+    DISCONNECT = "DISCONNECT"
+    BEFORE_1MIN = "BEFORE_1MIN"
+    BEFORE_3MIN = "BEFORE_3MIN"
+
+    def __init__(self, datetime, member, type):
         self.datetime = datetime
         self.member = member
+        self.type = type
 
     @property
     def datetime(self):
@@ -27,6 +31,16 @@ class Task:
         if not isinstance(value, (Member, User)):
             raise TypeError("type of 'member' must be Union[discord.Member, discord.User]")
         self.__member = value
+
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, value):
+        if value not in [Task.DISCONNECT, Task.BEFORE_1MIN, Task.BEFORE_3MIN]:
+            raise TypeError("type of 'type' must be Union[Task.DISCONNECT, Task.BEFORE_1MIN, Task.BEFORE_3MIN]")
+        self.__type = value
 
     def __lt__(self, other):
         if not isinstance(other, Task):
