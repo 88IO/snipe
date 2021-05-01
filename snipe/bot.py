57a8e18681e3f7fd99af30c80b5153ef9aefc17f
88IO <8810aoba@gmail.com>
2021-05-01 14:06:46 +0900

    権限関連修正

--- snipe/bot.py
+++ snipe/bot.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import traceback
 from .config import TOKEN
@@ -8,8 +9,8 @@ EXTENSIONS = [
 ]
 
 class Bot(commands.Bot):
-    def __init__(self, command_prefix):
-        super().__init__(command_prefix)
+    def __init__(self, command_prefix, intents):
+        super().__init__(command_prefix=command_prefix, intents=intents)
 
         for cog in EXTENSIONS:
             try:
@@ -26,7 +27,15 @@ class Bot(commands.Bot):
         raise error
 
 def main():
-    bot = Bot(command_prefix=commands.when_mentioned)
+    intents = discord.Intents.none()
+    intents.guilds = True
+    intents.members = True
+    intents.voice_states = True
+    intents.presences = True
+    intents.guild_messages = True
+    intents.guild_reactions = True
+
+    bot = Bot(command_prefix=commands.when_mentioned, intents=intents)
     bot.run(TOKEN)
 
 if __name__ == "__main__":
