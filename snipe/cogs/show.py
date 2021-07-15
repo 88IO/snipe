d8c0c203b28257de48558aca658b74f683de6a97
88IO <8810aoba@gmail.com>
2021-07-15 09:44:16 +0900

    管理者コマンドを削除

--- snipe/cogs/show.py
+++ snipe/cogs/show.py
@@ -25,21 +25,6 @@ class ShowCog(commands.Cog):
 
         await ctx.reply(embed=embed)
 
-    @commands.command()
-    @commands.is_owner()
-    async def showall(self, ctx):
-        for guild_id, tasks in self.tasks.items():
-            _guild = await self.bot.fetch_guild(guild_id)
-            embed = discord.Embed(title=_guild.name, description=_guild.id)
-
-            for task in tasks:
-                embed.add_field(
-                    name=f"{'強制切断' if task.type == Task.DISCONNECT else '3分前連絡'}: "
-                        + task.datetime.strftime("%m-%d %H:%M"),
-                    value=' '.join(map(lambda m: m.mention, task.members)))
-
-            await ctx.reply(embed=embed)
-
 
 def setup(bot):
     bot.add_cog(ShowCog(bot))
